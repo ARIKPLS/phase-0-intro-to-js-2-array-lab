845b80ab33800f2597bc337ef255f385a77e259a
EricVentura <ericvent@outlook.com>
2023-04-13 12:36:01 -0400

    test pass using arrow functions

--- index.js
+++ index.js
@@ -1,35 +1,18 @@
 // Write your solution here!
-const cats = ["Milo", "Otis", "Garfield"];
+const cats = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendCat(name) {
-    cats.push(name)
-}
+const destructivelyAppendCat = name => cats.push(name)
 
-function destructivelyPrependCat(name) {
-    cats.unshift(name)
-}
+const destructivelyPrependCat = name => cats.unshift(name)
 
-function destructivelyRemoveLastCat() {
-    cats.pop();
-}
+const destructivelyRemoveLastCat = name => cats.pop(name)
 
-function destructivelyRemoveFirstCat() {
-    cats.shift()
-}
+const destructivelyRemoveFirstCat = name => cats.shift(name)
 
-function appendCat(name) {
-  return [...cats, name]
-    
-}
+const appendCat = name => [...cats, name]
 
-function prependCat(name) {
-    return [name, ...cats]
-}
+const prependCat = name => [name, ...cats]
 
-function removeLastCat() {
-    return cats.slice(0, cats.length -1);
-}
+const removeLastCat = () => cats.slice(0, cats.length -1)
 
-function removeFirstCat() {
-    return cats.slice(1)
-}
+const removeFirstCat = () => cats.slice(1)
